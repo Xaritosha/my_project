cc3307d5c28d28139d1adf1072ec39aaa5f9d506
Никита <62386725+Xaritosha@users.noreply.github.com>
2020-03-26 17:21:59 +0300

    Add files via upload

---
+++ 26-03-2020/01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#define i 5 /* Число строк матрицы */
+#define j 10 /* Число столбцов матрицы */
+
+int main()
+{
+   int a[i][j];
+    int b[j]; /* Число минимумов = число стобцов */
+    int n, m;
+
+    for(n = 0; n < i; n++)
+        for(m = 0; m < j; m++)
+            a[n][m] = rand()%100;
+
+    for(n = 0; n < j; n++){
+        int min = INT_MAX;
+        for(m = 0; m < i; m++)
+        {
+            if(a[m][n] < min)
+            {
+                min = a[m][n];
+                b[n] = a[m][n];
+            }
+        }
+    }
+
+    for(n = 0; n < i; n++){
+        for(m = 0; m < j; m++)
+            printf("%5d", a[n][m]);
+        printf("\n");
+    }
+
+    for(n = 0; n < j; n++) printf("%d\t", b[n]);
+    return 0;
+}
